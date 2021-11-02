9035fe5f031e97a533341f1d125b15d6f19a87b4
Dflores101 <91530953+Dflores101@users.noreply.github.com>
2021-11-02 09:32:44 +0100

    Initial commit

---
+++ Assets/Script/PlayerControlle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // El coche se mueve hacia adelante
+        transform.Translate(10*Time.deltaTime*Vector3.forward);
+
+        //Coche rojo se mueve hacia adelante   
+        transform.Translate(10*Time.deltaTime*Vector3.forward
+            fgrrsgsrdg);
+    }
+}
